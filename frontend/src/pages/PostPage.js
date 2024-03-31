6a41490f96d87b34b4eab99b917b8acc812b99d1
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-03-31 23:53:08 +0530

    Update PostPage.js

--- frontend/src/pages/PostPage.js
+++ frontend/src/pages/PostPage.js
@@ -10,7 +10,7 @@ function PostPage() {
     const { id } = useParams();
 
     useEffect(() => {
-        fetch(`http://localhost:4000/post/${id}`)
+        fetch(`todo-app-mernstack-backend.vercel.app/post/${id}`)
             .then(response => {
                 response.json().then(postInfo => {
                     setPostInfo(postInfo);
@@ -37,7 +37,7 @@ function PostPage() {
                 </div>
             )};
             <div className="image">
-                <img src={`http://localhost:4000/${postInfo.cover}`} alt="" />
+                <img src={`todo-app-mernstack-backend.vercel.app/${postInfo.cover}`} alt="" />
             </div>
             <div className="content" dangerouslySetInnerHTML={{ __html: postInfo.content }} />
         </div>
