9a3aaa0aaa0263392fe3111eab164ae82796f57d
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-03-31 23:51:49 +0530

    Update CreatePost.js

--- frontend/src/pages/CreatePost.js
+++ frontend/src/pages/CreatePost.js
@@ -18,7 +18,7 @@ export default function CreatePost() {
         data.set('file',files[0]);
 
         ev.preventDefault();
-        const response = await fetch('todo-app-mernstack-backend.vercel.apppost', {
+        const response = await fetch('todo-app-mernstack-backend.vercel.app/post', {
             method: 'POST',
             body: data,
             credentials:'include',
