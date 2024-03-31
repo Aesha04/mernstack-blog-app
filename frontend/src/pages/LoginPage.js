0d4788f78ba8ad44498840eff7d3311ea1df027e
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-03-31 23:52:38 +0530

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ function LoginPage() {
     async function login(ev){
 
         ev.preventDefault();
-        const response = await fetch('http://localhost:4000/login',{
+        const response = await fetch('todo-app-mernstack-backend.vercel.app/login',{
             method: 'POST',
             body: JSON.stringify({username,password}),
             headers: {'Content-Type' : 'application/json'},
