8bbb55533c571c0c6ae03d75a3b68e2d29a70e73
Aesha04 <110104348+Aesha04@users.noreply.github.com>
2024-03-31 23:51:28 +0530

    Update IndexPage.js

--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -6,7 +6,7 @@ function IndexPage() {
     const [posts,setPosts] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:4000/post').then(response => {
+        fetch('todo-app-mernstack-backend.vercel.app/post').then(response => {
             response.json().then(posts => {
                 setPosts(posts);
             });
